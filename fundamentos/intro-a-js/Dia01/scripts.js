74a6b94cb3ef78595324b8e62bc1c73d69110353
Gustavo Oliveira <gustavo.h.oliveira7531@gmail.com>
2022-10-20 14:53:02 -0300

    JSPrimeirosPassos - Variaveis - Para fixar

---
+++ fundamentos/intro-a-js/Dia01/scripts.js
@@ -0,0 +1,10 @@
+let myName = "Gustavo";
+const birthCity = "Araraquara";
+let birthYear = 2001;
+console.log(myName, birthCity, birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
+// birthCity = "São Paulo";
+console.log(birthCity)
