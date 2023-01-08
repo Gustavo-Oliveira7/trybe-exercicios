18ce2c776095c6f1f35cb7ebe8f53d9aab7eafd5
Gustavo Oliveira <gustavo.h.oliveira7531@gmail.com>
2023-01-08 16:03:28 -0300

    hofs - Dia01 - exercicio - pessoaContratada

---
+++ fundamentos/hofs/Dia01/Exercicios - PessoaContratada.js
@@ -0,0 +1,14 @@
+const emailGenerator = (nome) => {
+   return nome.replace(' ', '_').toLowerCase() + '@trybe.com';
+};
+
+const newEmployees = () => {
+    const employees = {
+      id1: emailGenerator('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: emailGenerator('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: emailGenerator('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    }
+    return employees;
+  };
+
+console.log(newEmployees());
