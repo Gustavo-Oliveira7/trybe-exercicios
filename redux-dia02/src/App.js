290d6dd5803cbe78206f564f5b7897bed2b036b0
Gustavo Oliveira <gustavo.h.oliveira7531@gmail.com>
2023-03-04 13:19:53 -0300

    começando exercicios do dia02

---
+++ redux-dia02/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Contador</h1>
+      <h2>0</h2>
+      <button>Incrementador</button>
+    </div>
+  );
+}
+
+export default App;
